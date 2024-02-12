17e4e785360df237186d9249b01c6f01ebc67498
Oluwanifemi Ogidama <ogidamanifemi@gmail.com>
2024-02-12 11:10:22 -0500

    login page
    Created the login page

---
+++ CroKnitters/CroKnitters/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CroKnitters.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ CroKnitters/CroKnitters/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./Views/**/*.{cshtml,razor}", "./wwwroot.js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
